64b274b215cae0e48fd395a6f8d8c712ce9ada89
public-release <as-public-release@janestreet.com>
2024-08-23 21:33:26 +0100

    v0.18~preview.129.38+45

--- validation/src/validate_grammar.ml
+++ validation/src/validate_grammar.ml
@@ -67,7 +67,7 @@ let rec known_to_accept_all_sexps (grammar : Sexp_grammar.grammar) : bool =
 let validate_grammar ?test_count ?show_grammar:(should_show_grammar = true) (module M : S)
   =
   let config =
-    let%map.Option test_count = test_count in
+    let%map.Option test_count in
     { Base_quickcheck.Test.default_config with test_count }
   in
   if should_show_grammar then show_grammar (module M);
